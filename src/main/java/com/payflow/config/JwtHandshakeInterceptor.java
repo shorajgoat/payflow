50e87cc06c8bafa1d742ecca6782dbf9ac97f19b
Shoraj Ghimire <shorajghm10@gmail.com>
2026-09-20 17:30:42 +0545

    Create JwtHandshakeInterceptor.java

---
+++ src/main/java/com/payflow/config/JwtHandshakeInterceptor.java
@@ -0,0 +1,46 @@
+package com.payflow.config;
+
+import com.payflow.auth.JwtService;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+@Component
+public class JwtHandshakeInterceptor implements HandshakeInterceptor {
+
+    private final JwtService jwtService;
+
+    public JwtHandshakeInterceptor(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                    WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        if (request instanceof ServletServerHttpRequest servletRequest) {
+            String token = servletRequest.getServletRequest().getParameter("token");
+            if (token != null) {
+                try {
+                    String username = jwtService.extractUsername(token);
+                    if (!jwtService.isTokenExpired(token)) {
+                        attributes.put("username", username);
+                        return true;
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                WebSocketHandler wsHandler, Exception exception) {
+    }
+}
